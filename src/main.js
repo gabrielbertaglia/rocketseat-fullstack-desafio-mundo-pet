b84743f7217af9297906342a6831ad3d31f4a178
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2024-11-28 10:54:50 -0300

    feat: layout form new pet schedule

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@
 import "./libs/dayjs.js";
 
 // CSS
+import "./styles/form.css";
 import "./styles/global.css";
 import "./styles/input.css";
 import "./styles/schedule.css";
+
+//JS
+import "./modules/modal/index.js";
---
+++ src/modules/modal/index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("open-modal");
+const modal = document.querySelector("dialog");
+
+button.onclick = () => {
+  modal.showModal();
+};
+
+button;
